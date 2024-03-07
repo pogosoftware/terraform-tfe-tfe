8e86ca9da481f8d4d312c2b2152438a51d2ed1cd
Przemyslaw Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-03-07 11:19:29 +0100

    Removed aws credential variables

--- modules/workspace/locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  tfc_aws_provider_auth = var.aws_provider_auth == true ? 1 : 0
-}
--- modules/workspace/main.tf
+++ modules/workspace/main.tf
@@ -28,51 +28,6 @@ resource "tfe_workspace" "this" {
 ####################################################################################################
 ### VARIABLES
 ####################################################################################################
-resource "tfe_variable" "tfc_aws_provider_auth" {
-  count = local.tfc_aws_provider_auth
-
-  key          = "TFC_AWS_PROVIDER_AUTH"
-  value        = var.aws_provider_auth
-  category     = "env"
-  workspace_id = tfe_workspace.this.id
-}
-
-resource "tfe_variable" "tfc_aws_region" {
-  count = local.tfc_aws_provider_auth
-
-  key          = "AWS_REGION"
-  value        = var.aws_region
-  category     = "env"
-  workspace_id = tfe_workspace.this.id
-}
-
-resource "tfe_variable" "tfc_aws_workload_identity_audience" {
-  count = local.tfc_aws_provider_auth
-
-  key          = "TFC_AWS_WORKLOAD_IDENTITY_AUDIENCE"
-  value        = var.aws_workload_identity_audience
-  category     = "env"
-  workspace_id = tfe_workspace.this.id
-}
-
-resource "tfe_variable" "tfc_aws_plan_role_arn" {
-  count = local.tfc_aws_provider_auth
-
-  key          = "TFC_AWS_PLAN_ROLE_ARN"
-  value        = var.aws_plan_role_arn
-  category     = "env"
-  workspace_id = tfe_workspace.this.id
-}
-
-resource "tfe_variable" "tfc_aws_apply_role_arn" {
-  count = local.tfc_aws_provider_auth
-
-  key          = "TFC_AWS_APPLY_ROLE_ARN"
-  value        = var.aws_apply_role_arn
-  category     = "env"
-  workspace_id = tfe_workspace.this.id
-}
-
 resource "tfe_variable" "this" {
   for_each = var.workspace_variables
 
