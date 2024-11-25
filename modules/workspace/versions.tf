36a8e1206beddbd551f3e7ed6784b73375a44a04
Przemysław Pogorzelec <29803615+scrhicks@users.noreply.github.com>
2024-11-25 09:04:56 +0100

    Update version constraints (#110)

--- modules/workspace/versions.tf
+++ modules/workspace/versions.tf
@@ -1,14 +1,14 @@
 terraform {
-  required_version = ">= 1.9"
+  required_version = ">= 1.9.0, < 2.0.0"
 
   required_providers {
     hcp = {
       source  = "hashicorp/hcp"
-      version = "~> 0.99"
+      version = ">= 0.99.0, < 1.0.0"
     }
     tfe = {
       source  = "hashicorp/tfe"
-      version = "~> 0.60"
+      version = ">= 0.60.0, < 1.0.0"
     }
   }
 }
